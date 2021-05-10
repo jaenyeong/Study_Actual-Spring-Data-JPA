db252175d44bd1c69e33d2ad6497a84e27cd9a45
jaenyeong <jaenyeong.dev@gmail.com>
2021-05-10 15:17:00 +0900

    fix(BaseEntity at el): Fix createdBy, lastModifiedBy value is null in BaseEntity

--- src/main/java/com/jaenyeong/study_actualspringdatajpa/entity/BaseEntity.java
+++ src/main/java/com/jaenyeong/study_actualspringdatajpa/entity/BaseEntity.java
@@ -5,7 +5,9 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
 @Getter
 public abstract class BaseEntity extends BaseTimeEntity {
     @CreatedBy
